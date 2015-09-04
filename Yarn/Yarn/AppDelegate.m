fd17cc9789bda9ac378daad009fdaa33c22be625
javelin <doc.javelin@gmail.com>
2015-09-04 10:24:09 +0800

    Fixed #34

--- Yarn/Yarn/AppDelegate.m
+++ Yarn/Yarn/AppDelegate.m
@@ -46,6 +46,7 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     [_delegate saveData];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -73,6 +74,7 @@
     NSObject *storedObject = [userDefaults objectForKey:key];
     if (!storedObject) {
         [userDefaults setObject:object forKey:key];
+        [[NSUserDefaults standardUserDefaults] synchronize];
     }
 }
 
